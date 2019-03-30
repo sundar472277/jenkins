36796461c7cce5b60920f505002908953db3aa36
sundar <sundarit1994@gmail.com>
2019-03-30 15:35:08 +0530

    first commit

---
+++ StringCheck.java
@@ -0,0 +1,12 @@
+
+public class StringCheck {
+
+	public static void main(String[] args) {
+	  String S1 = new String("Sundar Munichamy");
+	  String S2 = "M";
+	  System.out.println(S1);
+	  System.out.println(S1.concat(S2));
+	  System.out.println(S1);
+
+	}
+}
